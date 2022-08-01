32718e012bafc0f860227b4e9d2faaf07f0f4930
Sandor Gonczi <sandorgonczi@outlook.com>
2022-08-01 16:24:12 +0100

    fixing recommendations

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const { getTopics, getArticleById } = require("./controllers/contoller");
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
@@ -22,7 +20,7 @@ app.use((err, req, res, next) => {
 
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
-    res.status(400).send({ msg: "Invalid Id, Id must be a number!" });
+    res.status(400).send({ msg: "Invalid Id!" });
   }
 });
 
