ac6a0d8267553aa4a7d43dffccb8166bd18859d5
Sandor Gonczi <sandorgonczi@outlook.com>
2022-08-02 10:31:11 +0100

    uptades /api/articles/:artile_id end-point so response object includes comment count

--- models/models.js
+++ models/models.js
@@ -7,17 +7,25 @@ exports.selectTopics = () => {
 };
 
 exports.selectArticleById = (article_id) => {
-  return db
-    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
-    .then(({ rows }) => {
-      if (!rows[0]) {
+  const prom1 = db.query("SELECT * FROM articles WHERE article_id = $1;", [
+    article_id,
+  ]);
+  const prom2 = db.query(
+    "SELECT COUNT(article_id) AS comment_count FROM comments WHERE article_id = $1;",
+    [article_id]
+  );
+  return Promise.all([prom1, prom2]).then(
+    ([{ rows: rows1 }, { rows: rows2 }]) => {
+      if (!rows1[0]) {
         return Promise.reject({
           status: 404,
           msg: "No Article exists with that Id!",
         });
       }
-      return rows[0];
-    });
+      rows2[0].comment_count = +rows2[0].comment_count;
+      return { ...rows1[0], ...rows2[0] };
+    }
+  );
 };
 
 exports.patchVoteById = (article_id, voteNum) => {
